28f64532eb2fbe843d2f81b351cca1fdd4c67edd
carlirom <carli.f.roman@gmail.com>
2022-08-11 20:32:02 -0400

    adding code

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -9,7 +9,14 @@ st.image(image, caption='Photo by Miesha Maiden from Pexels')
 st.text('The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.')
 st.text('In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.')
 
-code = '''def hello():
-    print("Hello, Streamlit!")'''
-
+st.header('T1. Walrus operator')
+st.text('The Walrus or := operator is one of the latest additions to python 3.8. It is an assignment operator that lets you assign value to a variable within an expression like conditional statements, loops, etc.')
+st.subheader('Example')
+code = '''
+Mylist = [1,2,3]
+if(l := len(mylist) > 2)
+print(l)'''
+st.code(code, language='python')
+st.subheader('Output')
+code = '''3'''
 st.code(code, language='python')
