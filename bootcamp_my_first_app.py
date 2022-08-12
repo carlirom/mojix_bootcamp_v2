67976d267a30bf38dbde28e6cc815f7becfac348
carlirom <carli.f.roman@gmail.com>
2022-08-11 20:20:50 -0400

    header

--- bootcamp_my_first_app.py
+++ bootcamp_my_first_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write("HELLO FROM THE MOJIX BOOTCAMP")
+st.title('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
+st.text('Simple but effective tips for every python lovers')
+st.text('The compactness of Python can make a developer’s life a lot easier when writing lines and lines of code. But there are some lesser-known Python tricks that can surprise you with their amazing capabilities.')
+st.text('In today’s article, I will discuss 10 Python tips and tricks that will be really helpful for beginners to write more compact code. Knowing these tips and tricks will definitely save you some valuable time.')
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+
+st.code(code, language='python')
